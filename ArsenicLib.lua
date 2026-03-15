35399e582e49e2f708e9d772900233c09aeefc61
Emperium <yoursecretnooby@gmail.com>
2026-03-15 11:24:57 +0800

    Add new modules to ArsenicLib

--- ArsenicLib.lua
+++ ArsenicLib.lua
@@ -7,16 +7,30 @@ local Window = require(Modules.Window)
 local Tabs = require(Modules.Tabs)
 local Elements = require(Modules.Elements)
 local Animations = require(Modules.Animations)
+local Layout = require(Modules.Layout)
+local Drag = require(Modules.Drag)
+local Blur = require(Modules.Blur)
+local Notifications = require(Modules.Notifications)
 local Config = require(Modules.Config)
 local KeySystem = require(Modules.KeySystem)
+local Scaling = require(Modules.Scaling)
+local Search = require(Modules.Search)
+local PluginLoader = require(Modules.PluginLoader)
 
 Arsenic.Modules = {
     Window = Window,
     Tabs = Tabs,
     Elements = Elements,
     Animations = Animations,
+    Layout = Layout,
+    Drag = Drag,
+    Blur = Blur,
+    Notifications = Notifications,
     Config = Config,
-    KeySystem = KeySystem
+    KeySystem = KeySystem,
+    Scaling = Scaling,
+    Search = Search,
+    PluginLoader = PluginLoader
 }
 
 Arsenic.Themes = {
